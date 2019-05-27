9a2623f7d4e4c386a8048496e5690d6ab807547b
vikirnt <vikrantgajria@gmail.com>
2019-05-27 10:45:57 +0530

    Complete JWT token generation part

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const _ = require('lodash');
 
 const database = knex(require('../knexfile'));
 
@@ -14,35 +15,42 @@ const checkPassword = (reqPassword, foundUser) => bcrypt.compare(
     foundUser.password_digest,
 );
 
-const createUser = async (user) => {
-  const data = await database.raw(
-      `INSERT INTO users (email, password_digest, token, created_at) 
-      VALUES (?, ?, ?, ?) 
-      RETURNING id, email, created_at, token`,
-      [user.email, user.password_digest, user.token, new Date()],
-  );
-  return data.rows[0];
-};
-
 const findUser = async (userReq) => {
   const data = await database.raw(
-      'SELECT * FROM users WHERE email = ?',
+      `SELECT * FROM users WHERE email = ?`,
       [userReq.email],
   );
   return data.rows[0];
 };
 
+const getRoles = async (user) => {
+  const data = await database.raw(
+      `SELECT role_name FROM users_roles WHERE user_id = ?`,
+      [user.id]
+  );
+  return data.rows.map((o) => o.role_name);
+};
+
 const createToken = async (user) => {
   const payload = {
-    'X-Hasura-default-role': 'admin',
+    'X-Hasura-default-role': 'user',
   };
+
+  const roles = await getRoles(user);
+  if (roles.find((e) => e === 'admin')) {
+    payload['X-Hasura-default-role'] = 'admin';
+    payload['X-Hasura-admin-secret'] = 'vikrant'; // from Hasura
+  }
+
+  payload['X-Hasura-allowed-roles'] = roles;
+
   return jwt.sign(payload, 'not-so-secret');
 };
 
 
 const updateUserToken = async (token, user) => {
   const data = await database.raw(
-      'UPDATE users SET token = ? WHERE id = ? RETURNING id, email, token',
+      `UPDATE users SET token = ? WHERE id = ? RETURNING id, email, token`,
       [token, user.id]
   );
   return data.rows[0];
@@ -66,7 +74,25 @@ exports.signIn = async (req, res) => {
   const token = await createToken(foundUser);
   await updateUserToken(token, foundUser);
   foundUser.token = token;
-  return res.status(200).json(foundUser);
+
+  const cleanUser = _.omit(foundUser, 'password_digest');
+  return res.status(200).json(cleanUser);
+};
+
+// ============================
+// This part isn't required as
+// a user with Admin role can
+// add users through Graphql
+// ============================
+
+const createUser = async (user) => {
+  const data = await database.raw(
+      `INSERT INTO users (email, password_digest, token, created_at) 
+      VALUES (?, ?, ?, ?) 
+      RETURNING id, email, created_at, token`,
+      [user.email, user.password_digest, user.token, new Date()],
+  );
+  return data.rows[0];
 };
 
 exports.signUp = async (req, res) => {
